e5c26c64665ca99b536d69136b69ac4e16473a84
Sate13720 <88797707+Sate13720@users.noreply.github.com>
2023-03-28 13:52:33 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,6 +1,7 @@
 from re import A
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
@@ -9,6 +10,11 @@ st.markdown('''
 # Exploratory Data Analysis Web Application
 ''')
 
+
+#How to upload file from PC
+with st.sidebar.header("Upload your dataset(.csv)"):
+    uploaded_file = st.sidebar.file_uploader("Upload your file", type=['csv'])
+    df = sns.load_dataset('iris')
 
 #Profiling report for pandas
 if uploaded_file is not None:
